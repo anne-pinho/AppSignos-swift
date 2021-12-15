4967c6d8281caeaca5016da72eb612913a068c85
anne-pinho <anne.pinhov@gmail.com>
2021-12-14 21:12:46 -0400

    Initial Commit

---
+++ AppSignos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AppSignos
+//
+//  Created by Carol Pinho on 14/12/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
